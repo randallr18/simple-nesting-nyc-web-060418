7d946cb719ecb88b01a797fd5de0ab8433dc0b73
Risher Randall <randallr18@mail.wlu.edu>
2018-06-01 16:23:06 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -114,6 +114,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
      
 end
@@ -136,5 +137,5 @@ def adding_to_dennis
      }
      
      programmer_hash[:dennis_ritchie][:languages].push('Assembly')
-
+programmer_hash
 end
