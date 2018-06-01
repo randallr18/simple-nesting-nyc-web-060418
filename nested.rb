6909d1687345eec79787091c80dd889594e0b65b
Risher Randall <randallr18@mail.wlu.edu>
2018-06-01 16:14:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,10 @@ def adding_matz
         }
      }
      
-     programmer_hash[:yukihiro_matsumoto] = 
+     programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }
 
     
 end
@@ -110,6 +113,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
      
 end
 
@@ -129,5 +134,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].push('Assembly')
 
 end
